7cfe20547901b9c978681be6f272edc328db1753
TonyLiu0430 <104262003+TonyLiu0430@users.noreply.github.com>
2025-05-05 22:19:26 +0800

    Matrix
    * add main function and compile option for generate assembly
    
    * [feat] add matrix class

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,43 @@
+#include <iostream>
+#include <functional>
+#include <map>
+#include <print>
+#include <matrix.h>
+
+
+
 int main(int argc, char **argv) {
+    Matrix<int> matrix(3, 3);
+
+    for(auto it = matrix.begin(); it != matrix.end(); ++it) {
+        for(auto it2 = (*it).begin(); it2 != (*it).end(); ++it2) {
+            std::cout << *it2 << " ";
+        }
+    }
+
+	std::cout << "\n";
+
+	for (auto x : matrix) {
+		for (auto &y : x) {
+            y = 1;
+		}
+	}
+
+    matrix[1, 2] = 3;
+
+	matrix[2][2] = 4;
+
+    for (auto x : matrix) {
+        for (auto y : x) {
+            std::cout << y << " ";
+        }
+        std::cout << "\n";
+    }
+
+    std::print("{}", matrix);
+
+	std::cout << "======================\n";
+
+	std::cout << matrix << "\n";
 }
+

2f76b9fcd4084f6726f42a47feebf5e5798330e2
billy <billy948787@gmail.com>
2025-05-07 08:52:48 +0800

    add clang format

--- include/matrix.h
+++ include/matrix.h
@@ -1,226 +1,201 @@
 #pragma once
-#include <cstddef>
 #include <compare>
+#include <cstddef>
 #include <format>
+#include <iostream>
 
-template<typename T>
+template <typename T>
 class Row {
-public:
-    auto begin() const {
-        return data;
-    }
-    auto end() const {
-        return data + cols;
-    }
-	const T& operator[](size_t j) const {
-        #ifdef DEBUG
-		if (j >= cols) {
-			throw std::out_of_range("Index out of range");
-		}
-        #endif
-		return data[j];
-	}
-	T& operator[](size_t j) {
-        #ifdef DEBUG
+   public:
+    auto begin() const { return data; }
+    auto end() const { return data + cols; }
+    const T &operator[](size_t j) const {
+#ifdef DEBUG
         if (j >= cols) {
             throw std::out_of_range("Index out of range");
         }
-        #endif
+#endif
         return data[j];
     }
-    Row(T * const data, size_t cols): data(data), cols(cols) {}
-    Row(const Row& other): data(other.data), cols(other.cols) {}
-private:
-    T* const data = nullptr;
+    T &operator[](size_t j) {
+#ifdef DEBUG
+        if (j >= cols) {
+            throw std::out_of_range("Index out of range");
+        }
+#endif
+        return data[j];
+    }
+    Row(T *const data, size_t cols) : data(data), cols(cols) {}
+    Row(const Row &other) : data(other.data), cols(other.cols) {}
+
+   private:
+    T *const data = nullptr;
     const size_t cols = 0;
 };
 
-template<typename T>
+template <typename T>
 class RowIterator {
-private:
+   private:
     T *data;
     size_t size;
-public:
-    RowIterator(T* _data, size_t _size): data(_data), size(_size) {}
+
+   public:
+    RowIterator(T *_data, size_t _size) : data(_data), size(_size) {}
 
     // ++x
-    RowIterator operator ++() {
+    RowIterator operator++() {
         data += size;
         return *this;
     }
 
     // x++
-    RowIterator operator ++(int) {
+    RowIterator operator++(int) {
         auto temp = *this;
         data += size;
         return temp;
     }
 
-    RowIterator operator --() {
+    RowIterator operator--() {
         auto temp = *this;
         data -= size;
         return temp;
     }
 
-    RowIterator operator --(int) {
+    RowIterator operator--(int) {
         auto temp = *this;
         data -= size;
         return temp;
     }
 
-    RowIterator operator +(ptrdiff_t offset) const {
+    RowIterator operator+(ptrdiff_t offset) const {
         return RowIterator(data + offset, size);
     }
 
-    RowIterator operator -(ptrdiff_t offset) const {
+    RowIterator operator-(ptrdiff_t offset) const {
         return RowIterator(data - offset, size);
     }
 
-    auto operator == (const RowIterator& other) const {
-        #ifdef DEBUG
+    auto operator==(const RowIterator &other) const {
+#ifdef DEBUG
         if (size != other.size) {
             throw std::runtime_error("RowIterator sizes do not match");
         }
-        #endif
+#endif
         return data == other.data;
     }
 
-    auto operator != (const RowIterator& other) const {
-        #ifdef DEBUG
+    auto operator!=(const RowIterator &other) const {
+#ifdef DEBUG
         if (size != other.size) {
             throw std::runtime_error("RowIterator sizes do not match");
         }
-        #endif
+#endif
         return data != other.data;
     }
 
-    const Row<T> operator *() const {
-        return Row<T>(data, size);
-    }
+    const Row<T> operator*() const { return Row<T>(data, size); }
 
-	Row<T> operator *() {
-		return Row<T>(data, size);
-	}
+    Row<T> operator*() { return Row<T>(data, size); }
 };
 
-
-
-template<typename T>
+template <typename T>
 class Matrix {
-public:
-    const RowIterator<T>& begin() {
-        return RowIterator(data, cols);
-    }
-    const RowIterator<T>& end() {
+   public:
+    const RowIterator<T> begin() { return RowIterator(data, cols); }
+    const RowIterator<T> end() {
         return RowIterator(data + (rows * cols), cols);
     }
 
-    const T& operator[](size_t i, size_t j) const {
-        #ifdef DEBUG
-		if (i >= rows || j >= cols) {
-			throw std::out_of_range("Index out of range");
-		}
-        #endif
-		return data[i * cols + j];
-    }
-
-	T& operator[](size_t i, size_t j) {
-        #ifdef DEBUG
+    const T &operator[](size_t i, size_t j) const {
+#ifdef DEBUG
         if (i >= rows || j >= cols) {
             throw std::out_of_range("Index out of range");
         }
-        #endif
+#endif
         return data[i * cols + j];
     }
 
-	const Row<T> operator [](size_t i) const {
-        #ifdef DEBUG
-		if (i >= rows) {
-			throw std::out_of_range("Index out of range");
-		}
-        #endif
-        
-		return Row<T>(data + (i * cols), cols);
-	}
+    T &operator[](size_t i, size_t j) {
+#ifdef DEBUG
+        if (i >= rows || j >= cols) {
+            throw std::out_of_range("Index out of range");
+        }
+#endif
+        return data[i * cols + j];
+    }
 
-	Row<T> operator [](size_t i) {
-        #ifdef DEBUG
+    const Row<T> operator[](size_t i) const {
+#ifdef DEBUG
         if (i >= rows) {
             throw std::out_of_range("Index out of range");
         }
-        #endif
+#endif
+
         return Row<T>(data + (i * cols), cols);
     }
 
-	int row() const {
-		return rows;
-	}
+    Row<T> operator[](size_t i) {
+#ifdef DEBUG
+        if (i >= rows) {
+            throw std::out_of_range("Index out of range");
+        }
+#endif
+        return Row<T>(data + (i * cols), cols);
+    }
 
-	int col() const {
-		return cols;
-	}
+    int row() const { return rows; }
+
+    int col() const { return cols; }
 
     Matrix(int rows, int cols) : rows(rows), cols(cols) {
         data = new T[rows * cols]{};
     }
-    Matrix(const Matrix&) = delete;
-    Matrix& operator=(const Matrix&) = delete;
-    Matrix(Matrix&& other) : rows(other.rows), cols(other.cols), data(other.data) {
+    Matrix(const Matrix &) = delete;
+    Matrix &operator=(const Matrix &) = delete;
+    Matrix(Matrix &&other)
+        : rows(other.rows), cols(other.cols), data(other.data) {
         other.data = nullptr;
-		other.rows = -1;
-		other.cols = -1;
+        other.rows = -1;
+        other.cols = -1;
     }
     /*
-	{{1, 2, 3},
-	 {4, 5, 6},
-	 {7, 8, 9}}
+        {{1, 2, 3},
+         {4, 5, 6},
+         {7, 8, 9}}
     */
-    Matrix(std::initializer_list<std::initializer_list<T>>&& list) {
+    Matrix(std::initializer_list<std::initializer_list<T>> &&list) {
         rows = list.size();
-        for (auto& row : list) {
-			cols = std::max(cols, row.size());
+        for (auto &row : list) {
+            cols = std::max(cols, row.size());
         }
         data = new T[rows * cols]{};
         int i = 0;
-        for (auto& row : list) {
+        for (auto &row : list) {
             std::copy(row.begin(), row.end(), data + (i * cols));
             i++;
         }
     }
-    ~Matrix() {
-        delete[] data;
-    }
-private:    
-    T* data = nullptr;
+    ~Matrix() { delete[] data; }
+
+   private:
+    T *data = nullptr;
     int rows, cols;
 };
 
-std::ostream& operator<<(std::ostream& os, const Matrix<int>& matrix) {
-	for (int i = 0; i < matrix.row(); i++) {
-		for (int j = 0; j < matrix.col(); j++) {
-			os << matrix[i][j] << " ";
-		}
-		os << "\n";
-	}
-	return os;
-}
-
-
-template <typename T>
-struct std::formatter<Matrix<T>> {
-    constexpr auto parse(auto& ctx) {
-        auto it = ctx.begin();
-        auto end = ctx.end();
-
-        if (it != end && *it != '}') {
-            throw std::format_error("invalid format for Matrix");
+std::ostream &operator<<(std::ostream &os, const Matrix<int> &matrix) {
+    for (int i = 0; i < matrix.row(); i++) {
+        for (int j = 0; j < matrix.col(); j++) {
+            os << matrix[i][j] << " ";
         }
-
-        return end;
+        os << "\n";
     }
-
-    auto format(const Matrix<T>& matrix, auto& ctx) const {
-		auto out = ctx.out();
+    return os;
+}
+namespace std {
+template <typename T>
+struct formatter<Matrix<T>, char> : formatter<std::string, char> {
+    auto format(const Matrix<T> &matrix, auto &ctx) const {
+        auto out = ctx.out();
         for (int i = 0; i < matrix.row(); i++) {
             for (int j = 0; j < matrix.col(); j++) {
                 out = std::format_to(out, "{} ", matrix[i][j]);
@@ -230,6 +205,7 @@ struct std::formatter<Matrix<T>> {
         return out;
     }
 };
+};  // namespace std
 
 /*
     Matrix mtx;

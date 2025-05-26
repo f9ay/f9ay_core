9d5cd22d1ebe7e628c1ec43e01953c4db7e52e27
billy <billy948787@gmail.com>
2025-05-26 23:54:25 +0800

    change deflate code make it shorter

--- include/filter.hpp
+++ include/filter.hpp
@@ -8,11 +8,10 @@ enum class FilterType { None, Sub, Up, Average, Paeth };
 
 namespace f9ay::deflate {
 template <typename T>
-T paethPredictor(T left, T above, T upperLeft) {
+[[nodiscard]] T paethPredictor(T left, T above, T upperLeft) {
     // in theory the T will only one byte
 
-    int p = static_cast<int>(left) + static_cast<int>(above) -
-            static_cast<int>(upperLeft);
+    int p = static_cast<int>(left) + static_cast<int>(above) - static_cast<int>(upperLeft);
 
     int pa = std::abs(static_cast<int>(left) - p);
     int pb = std::abs(static_cast<int>(above) - p);
@@ -27,7 +26,7 @@ T paethPredictor(T left, T above, T upperLeft) {
 }
 
 template <MATRIX_CONCEPT InputMatrix>
-inline Matrix<std::decay_t<decltype(std::declval<InputMatrix>()[0, 0])>> filter(
+[[nodiscard]] inline Matrix<std::decay_t<decltype(std::declval<InputMatrix>()[0, 0])>> filter(
     const InputMatrix& matrix, FilterType filterType) {
     using T_val = std::decay_t<decltype(matrix[0, 0])>;
     f9ay::Matrix<T_val> filteredMatrix(matrix.row(), matrix.col());
@@ -38,13 +37,9 @@ inline Matrix<std::decay_t<decltype(std::declval<InputMatrix>()[0, 0])>> filter(
             T_val currentByte = matrix[i, j];
 
             // Raw(x-bpp): byte at same component in previous pixel
-            T_val left = (j == 0) ? static_cast<T_val>(0)
-                                  : matrix[i, j - 1];  // byte to the left
-            T_val above = (i == 0) ? static_cast<T_val>(0)
-                                   : matrix[i - 1, j];  // byte above
-            T_val upperLeft = (i == 0 || j == 0)
-                                  ? static_cast<T_val>(0)
-                                  : matrix[i - 1, j - 1];  // byte upper-left
+            T_val left = (j == 0) ? static_cast<T_val>(0) : matrix[i, j - 1];                     // byte to the left
+            T_val above = (i == 0) ? static_cast<T_val>(0) : matrix[i - 1, j];                    // byte above
+            T_val upperLeft = (i == 0 || j == 0) ? static_cast<T_val>(0) : matrix[i - 1, j - 1];  // byte upper-left
 
             T_val filteredByteVal;
 
@@ -68,9 +63,8 @@ inline Matrix<std::decay_t<decltype(std::declval<InputMatrix>()[0, 0])>> filter(
                         b -= (f + j) / 2;
                         c -= (g + k) / 2;
                         d -= (h + l) / 2;
-                        filteredByteVal = {
-                            static_cast<uint8_t>(a), static_cast<uint8_t>(b),
-                            static_cast<uint8_t>(c), static_cast<uint8_t>(d)};
+                        filteredByteVal = {static_cast<uint8_t>(a), static_cast<uint8_t>(b), static_cast<uint8_t>(c),
+                                           static_cast<uint8_t>(d)};
                     } else if (sizeof(T_val) == 3) {
                         auto [a, b, c] = currentByte;
                         auto [e, f, g] = left;
@@ -79,9 +73,7 @@ inline Matrix<std::decay_t<decltype(std::declval<InputMatrix>()[0, 0])>> filter(
                         a -= (e + i) / 2;
                         b -= (f + j) / 2;
                         c -= (g + k) / 2;
-                        filteredByteVal = {static_cast<uint8_t>(a),
-                                           static_cast<uint8_t>(b),
-                                           static_cast<uint8_t>(c)};
+                        filteredByteVal = {static_cast<uint8_t>(a), static_cast<uint8_t>(b), static_cast<uint8_t>(c)};
                     } else {
                         throw std::runtime_error("Unsupported color type");
                     }
@@ -98,9 +90,8 @@ inline Matrix<std::decay_t<decltype(std::declval<InputMatrix>()[0, 0])>> filter(
                         b -= paethPredictor(f, j, n);
                         c -= paethPredictor(g, k, o);
                         d -= paethPredictor(h, l, p);
-                        filteredByteVal = {
-                            static_cast<uint8_t>(a), static_cast<uint8_t>(b),
-                            static_cast<uint8_t>(c), static_cast<uint8_t>(d)};
+                        filteredByteVal = {static_cast<uint8_t>(a), static_cast<uint8_t>(b), static_cast<uint8_t>(c),
+                                           static_cast<uint8_t>(d)};
                     } else if constexpr (sizeof(T_val) == 3) {
                         auto [a, b, c] = currentByte;
                         auto [e, f, g] = left;
@@ -110,9 +101,7 @@ inline Matrix<std::decay_t<decltype(std::declval<InputMatrix>()[0, 0])>> filter(
                         a -= paethPredictor(e, i, m);
                         b -= paethPredictor(f, j, n);
                         c -= paethPredictor(g, k, o);
-                        filteredByteVal = {static_cast<uint8_t>(a),
-                                           static_cast<uint8_t>(b),
-                                           static_cast<uint8_t>(c)};
+                        filteredByteVal = {static_cast<uint8_t>(a), static_cast<uint8_t>(b), static_cast<uint8_t>(c)};
                     } else {
                         throw std::runtime_error("Unsupported color type");
                     }
